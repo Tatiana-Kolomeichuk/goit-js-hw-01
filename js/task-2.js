e3c0d977d36f9b6fc0ffd54c6fd084307bdf2e12
Tatiana <tany663@gmail.com>
2025-09-10 19:54:38 +0300

    1

--- js/task-2.js
+++ js/task-2.js
@@ -6,12 +6,3 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-
-
-const getShippingMessage = (country, price, deliveryFee) => 
-`Shipping to ${country} will cost ${price + deliveryFee} credits`;
-
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20));
